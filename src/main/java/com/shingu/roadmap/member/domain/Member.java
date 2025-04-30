10009eaa485d5cf5f2789c5fd87f6e28633404fd
Jupiter <kjimin2123@gmail.com>
2025-04-30 12:17:34 +0900

    feat(member): Refactor Member class and add Profile class
    - Member 클래스를 BaseEntity로 확장하여 기본 엔티티 기능을 추가했습니다.
    - Profile 클래스를 새로 생성하여 학력, 전공, 희망 직무 정보를 포함했습니다.
    - Member 클래스의 기술 및 자격증 목록을 유지하며, 국가직무능력표준 코드 목록을 추가했습니다.

--- src/main/java/com/shingu/roadmap/member/domain/Member.java
+++ src/main/java/com/shingu/roadmap/member/domain/Member.java
@@ -1,20 +1,41 @@
 package com.shingu.roadmap.member.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.shingu.roadmap.common.domain.BaseEntity;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Getter
-public class Member {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+public class Member extends BaseEntity {
+    @Column
+    private String name;
+
+    @Column
+    private String phoneNumber;
+
+    @Embedded
+    private Profile profile;
+
+    @ElementCollection
+    @CollectionTable(name = "member_skill", joinColumns = @JoinColumn(name = "member_id"))
+    @Column(name = "skill")
+    private Set<String> skills = new HashSet<>(); // 보유 기술 목록
+
+    @ElementCollection
+    @CollectionTable(name = "member_certificate", joinColumns = @JoinColumn(name = "member_id"))
+    @Column(name = "certificate")
+    private Set<String> certificates = new HashSet<>();; // 자격증 목록
+
+    @ElementCollection
+    @CollectionTable(name = "member_ncs", joinColumns = @JoinColumn(name = "member_id"))
+    @Column(name = "ncs_code")
+    private Set<String> ncsCodes = new HashSet<>();; // 국가직무능력표준 코드 목록
 }
