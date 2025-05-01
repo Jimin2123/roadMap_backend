391e97a7c2423392532990c073be32e717f2f8cc
Jupiter <kjimin2123@gmail.com>
2025-05-01 13:28:54 +0900

    feat(member): Update profile with NCS code recommendation
    - 프로필 업데이트 시 NCS 코드 추천 기능을 추가했습니다.
    - OpenAiService를 사용하여 추천된 NCS 코드를 가져오고,
    - 이를 멤버의 NCS 코드에 업데이트합니다.

--- src/main/java/com/shingu/roadmap/member/service/MemberService.java
+++ src/main/java/com/shingu/roadmap/member/service/MemberService.java
@@ -2,7 +2,6 @@ package com.shingu.roadmap.member.service;
 
 import com.shingu.roadmap.apis.openai.service.OpenAiService;
 import com.shingu.roadmap.member.domain.Member;
-import com.shingu.roadmap.member.domain.Profile;
 import com.shingu.roadmap.member.dto.request.ProfileRequest;
 import com.shingu.roadmap.member.dto.response.MemberResponse;
 import com.shingu.roadmap.member.repository.MemberRepository;
@@ -10,14 +9,15 @@ import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
 import java.util.Set;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class MemberService {
     private final MemberRepository memberRepository;
+    private final OpenAiService openAiService;
 
     @Transactional
     public MemberResponse updateProfile(Long memberId, ProfileRequest request) {
@@ -26,6 +26,17 @@ public class MemberService {
                 .orElseThrow(() -> new EntityNotFoundException("Member not found"));
 
         member.applyProfile(request);
+
+        // 무자비한 AI를 이용을 막기 위해 검토
+        if(!CollectionUtils.isEmpty(request.skills()) ||
+                !CollectionUtils.isEmpty(request.certificates())) {
+            Set<String> ncsCodes = openAiService.recommendNcsCodes(member).block();
+
+            // 추천된 NCS 코드가 실제 존재하는 코드인지 검증 하는 단계를 추가 해야함
+            // NCS 직무정보 API를 활용해 검증할 수 있음
+
+            member.updateNcsCodes(ncsCodes);
+        }
 
         return MemberResponse.from(member);
     }
