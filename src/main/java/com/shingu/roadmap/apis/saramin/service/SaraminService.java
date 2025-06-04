c45791d9a679126155f24135e68573ed6bb31d89
Jupiter <kjimin2123@gmail.com>
2025-06-05 01:59:10 +0900

    feat(task): Enhance job retrieval functionality for members
    - 회원 ID를 기반으로 사용자 맞춤 채용 정보를 조회하는 기능을 추가했습니다.
    
    - 사용자의 프로필과 주소 정보를 활용하여 지역에 맞는 채용 정보를 제공합니다.

--- src/main/java/com/shingu/roadmap/apis/saramin/service/SaraminService.java
+++ src/main/java/com/shingu/roadmap/apis/saramin/service/SaraminService.java
@@ -1,21 +1,45 @@
 package com.shingu.roadmap.apis.saramin.service;
 
+import com.shingu.roadmap.apis.openai.service.OpenAiService;
 import com.shingu.roadmap.apis.saramin.client.SaraminClient;
+import com.shingu.roadmap.apis.saramin.domain.SaraminJob;
+import com.shingu.roadmap.apis.saramin.domain.SaraminJobGroup;
+import com.shingu.roadmap.apis.saramin.domain.SaraminRegion;
 import com.shingu.roadmap.apis.saramin.dto.response.SaraminJobListResponse;
+import com.shingu.roadmap.apis.saramin.repository.SaraminRegionRepository;
+import com.shingu.roadmap.common.enums.EducationLevelType;
+import com.shingu.roadmap.member.domain.Profile;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class SaraminService {
 
+  private final SaraminRegionRepository saraminRegionRepository;
   private final SaraminClient saraminClient;
+  private final OpenAiService openAiService;
 
-  public SaraminJobListResponse getJobList(String keyword) {
-    return saraminClient.getJobList(keyword);
+  public SaraminJobListResponse getJobListForMember(int page, String regionName, Profile profile) {
+    SaraminRegion region = saraminRegionRepository.findFirstByName(regionName)
+        .orElseThrow(() -> new IllegalArgumentException("Region not found: " + regionName));
+
+//    Set<String> keyword = openAiService.generateKeyword(profile).block();
+
+    Set<Integer> jobCodes = profile.getDesiredJobs().stream().map(SaraminJob::getCode).collect(Collectors.toSet());
+    List<SaraminJobGroup> groupCodeList = profile.getDesiredJobs().stream().map(SaraminJob::getGroup).toList();
+    Set<Integer> groupCodes = groupCodeList.stream().map(SaraminJobGroup::getCode).collect(Collectors.toSet());
+
+    EducationLevelType educationLevel = EducationLevelType.valueOf(profile.getEducationLevel());
+
+    return saraminClient.getJobList(null, page, region, groupCodes, jobCodes, educationLevel);
   }
 
-  public SaraminJobListResponse getJobListForMainPage() {
-    return saraminClient.getJobList("");
+  public SaraminJobListResponse getJobListForMainPage(int page) {
+    return saraminClient.getJobList(null, page, null, null, null, null);
   }
 }
--- src/main/java/com/shingu/roadmap/task/controller/TaskController.java
+++ src/main/java/com/shingu/roadmap/task/controller/TaskController.java
@@ -5,6 +5,8 @@ import com.shingu.roadmap.task.service.TaskService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,8 +16,21 @@ public class TaskController implements TaskControllerSwagger {
 
   @Override
   @GetMapping("/api/v1/jobs")
-  public ResponseEntity<SaraminJobListResponse> getJobList() {
-    return null;
+  public ResponseEntity<SaraminJobListResponse> getJobList(
+          @RequestParam(name = "page", defaultValue = "0") int page
+  ) {
+    SaraminJobListResponse response = taskService.getJobListForMainPage(page);
+    return ResponseEntity.ok(response);
+  }
+
+  @Override
+  @GetMapping("/api/v1/jobs/{memberId}")
+  public ResponseEntity<SaraminJobListResponse> getJobListForMember(
+          @RequestParam(name = "page", defaultValue = "0") int page,
+          @PathVariable Long memberId
+  ) {
+    SaraminJobListResponse response = taskService.getJobListForMember(page, memberId);
+    return ResponseEntity.ok(response);
   }
 
   @Override
