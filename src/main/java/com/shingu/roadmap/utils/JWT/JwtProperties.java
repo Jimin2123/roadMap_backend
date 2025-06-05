552857d99c4365c1b229824ef70139ae002f25eb
Jupiter <kjimin2123@gmail.com>
2025-06-05 22:16:39 +0900

    feat(jwt): Add JWT configuration and utility classes
    - JWT 관련 설정 및 유틸리티 클래스를 추가했습니다.
    - JWT 비밀 키, 발급자, 액세스 및 리프레시 토큰의 속성을 정의했습니다.
    - JWT 토큰 생성 및 검증 기능을 구현했습니다.

---
+++ src/main/java/com/shingu/roadmap/utils/JWT/JwtProperties.java
@@ -0,0 +1,43 @@
+package com.shingu.roadmap.utils.JWT;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.annotation.Validated;
+
+@Validated
+@Configuration
+@ConfigurationProperties(prefix = "jwt")
+@Data
+public class JwtProperties {
+
+  @NotBlank
+  private String secretKey;
+
+  @NotBlank
+  private String issuer;
+
+  @Valid
+  private TokenProperties accessToken;
+
+  @Valid
+  private TokenProperties refreshToken;
+
+  @NotBlank
+  private String audience;
+
+  @Getter
+  @Setter
+  public static class TokenProperties {
+    @NotBlank
+    private String secretKey;
+
+    @NotNull
+    private Long expiration;
+  }
+}
---
+++ src/main/java/com/shingu/roadmap/utils/JWT/JwtUtil.java
@@ -0,0 +1,75 @@
+package com.shingu.roadmap.utils.JWT;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+  private final JwtProperties jwtProperties;
+
+  public JwtUtil(JwtProperties jwtProperties) {
+    this.jwtProperties = jwtProperties;
+  }
+
+  public String generateToken(String tokenType, TokenPayload payload) {
+
+    JwtProperties.TokenProperties tokenProperties =
+            "access".equals(tokenType) ? jwtProperties.getAccessToken() : jwtProperties.getRefreshToken();
+
+    Key key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(tokenProperties.getSecretKey()));
+
+    Claims claims = Jwts.claims();
+    claims.put("memberId", payload.memberId());
+    claims.put("email", payload.email());
+    claims.put("name", payload.name());
+    claims.put("role", payload.role()); //USER, ADMIN
+
+    return Jwts.builder()
+            .setClaims(claims)
+            .setSubject(payload.memberId().toString())
+            .setIssuer(jwtProperties.getIssuer())
+            .setAudience(jwtProperties.getAudience())
+            .setIssuedAt(new Date())
+            .setExpiration(new Date(System.currentTimeMillis() + tokenProperties.getExpiration()))
+            .signWith(key, SignatureAlgorithm.HS256)
+            .compact();
+  }
+
+  public String generateAccessToken(TokenPayload payload) {
+    return generateToken("access", payload);
+  }
+
+  public String generateRefreshToken(TokenPayload payload) {
+    return generateToken("refresh", payload);
+  }
+
+  public String verifyAccessToken(String accessToken) {
+    return parseClaims("access", accessToken).getSubject();
+  }
+
+  public String verifyRefreshToken(String refreshToken) {
+    return parseClaims("refresh", refreshToken).getSubject();
+  }
+
+  public Claims parseClaims(String tokenType,String token) {
+    String secretKey = "access".equals(tokenType)
+            ? jwtProperties.getAccessToken().getSecretKey()
+            : jwtProperties.getRefreshToken().getSecretKey();
+
+    Key key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey));
+
+    return Jwts.parserBuilder()
+            .setSigningKey(key)
+            .build()
+            .parseClaimsJws(token)
+            .getBody();
+  }
+}
